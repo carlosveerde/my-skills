ec8780fdbf8354d1f4365461c39767a2b1fcb390
Verde <carlos-verde@outlook.com>
2024-01-23 11:37:46 -0300

    feat: :sparkles: Add new state to compose list of skills. also commentary in the code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
 import {
   Text,
@@ -6,11 +7,18 @@ import {
   TextInput,
   Platform,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 
 export function Home() {
-  const [newSkill, setNewSkill] = useState('');
-
+  // States
+  const [newSkill, setNewSkill] = useState(); // newSkill is the value of the input, setNewSkill is the function to change the value of newSkill as the user inputs.
+  const [mySkills, setMySkills] = useState([]); // mySkills is the array of skills, setMySkills is the function to change the value of mySkills as the user adds a new skill.
+  // Functions
+  function handleAddNewSkill() {
+    setMySkills([...mySkills, newSkill]); // The spread operator is used to add the newSkill to the mySkills array. Also coud be used: setMySkills(anything => [...anything, newSkill]);
+  }
+  let skillKey = 0; // Used to give a unique key to each skill button.
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome, Carlos!</Text>
@@ -20,14 +28,26 @@ export function Home() {
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
       />
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Text style={styles.buttonText}>Adicionar habilidade</Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={handleAddNewSkill}>
+        <Text style={styles.buttonText}>Add Skill</Text>
       </TouchableOpacity>
-      <Text style={[styles.text, {marginTop: 50}]}>{newSkill}</Text>
+      <Text style={[styles.text, {marginTop: 50}]}>My Skills</Text>
+      <ScrollView persistentScrollbar>
+        {/* Map through the mySkills Array and render the button for each element */}
+        {mySkills.map(skill => (
+          <TouchableOpacity style={styles.skillButton} key={skillKey++}>
+            <Text style={styles.skillButtonText}>{skill}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
+// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -57,6 +77,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  skillButton: {
+    backgroundColor: '#1f1e25',
+    padding: 15,
+    borderRadius: 50,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  skillButtonText: {
     color: '#fff',
     fontSize: 17,
     fontWeight: 'bold',
